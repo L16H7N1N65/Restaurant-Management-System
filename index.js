9deaf0c6dc84a48a33e4fd79a037806bfc35b9e6
L16H7N1N65 <meg.linda@hotmail.fr>
2024-06-11 15:02:56 +0200

    adding index.js

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 //index.js
 
+const express = require('express');
+const app = express();
+const routes = require('../routes'); 
+app.use('/api', routes);
 
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT;
-
-const employeesRoutes = require('./employeesRoutes');
-const restaurantRoutes = require('./restaurantRoutes');
-
-module.exports = {
-  employeesRoutes,
-  restaurantRoutes
-};
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="/path/to/your/css/styles.css" rel="stylesheet">
-</head>
-<body class="bg-gray-200 text-gray-800">
-    <header class="bg-blue-500 text-white p-4">
-        <h1>Header</h1>
-    </header>
-
-    <main class="p-4">
-        
-    </main>
-
-    <footer class="bg-blue-500 text-white p-4">
-        <p>Footer</p>
-    </footer>
-</body>
-</html>
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
